437327f524a42f5023d1ae967b3b05ae8d388870
nos <nos@kmc.gr.jp>
2019-09-30 22:21:51 +0900

    表示を整えて母数を表示

--- content.js
+++ content.js
@@ -1,7 +1,8 @@
 table = document.querySelector(".standard_list")
 head = table.querySelector("tr.th_normal")
 label = document.createElement("th")
-label.textContent = "謎の数字(%)"
+label.style.whiteSpace = "pre"
+label.textContent = "単位取得率(%)\n(取得/登録)"
 head.appendChild(label)
 
 Promise.all([
@@ -22,7 +23,7 @@ fetch(chrome.runtime.getURL("./2017.json"))])
         })
         }
         if(!item) return
-        td.textContent = item["単位取得率"]
+        td.textContent = String(item["単位取得率"]).substr(0, 4) + "(" + item["単位取得者数（平均）"] + "/" + item["履修登録者数（平均）"] + ")"
         tr.appendChild(td)
     })
 })
