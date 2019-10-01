bf0b8060aa756de1c1f3bcf48acb3ef54c5bed5c
nos <nos@kmc.gr.jp>
2019-10-01 10:01:58 +0900

    クソ雑にtimeslot/topからのリダイレクト処理

--- content.js
+++ content.js
@@ -1,29 +1,34 @@
-table = document.querySelector(".standard_list")
-head = table.querySelector("tr.th_normal")
-label = document.createElement("th")
-label.style.whiteSpace = "pre"
-label.textContent = "単位取得率(%)\n(取得/登録)"
-head.appendChild(label)
+if(location.href === "https://www.k.kyoto-u.ac.jp/student/la/timeslot/top"){
+    location.href = "https://www.k.kyoto-u.ac.jp/student/la/timeslot/timeslot_list"
+}
+else{
+    table = document.querySelector(".standard_list")
+    head = table.querySelector("tr.th_normal")
+    label = document.createElement("th")
+    label.style.whiteSpace = "pre"
+    label.textContent = "単位取得率(%)\n(取得/登録)"
+    head.appendChild(label)
 
-Promise.all([
-fetch(chrome.runtime.getURL("./2018.json")),
-fetch(chrome.runtime.getURL("./2017.json"))])
-.then(([res1, res2])=>{
-    return Promise.all([res1.json(), res2.json()])
-})
-.then(([json1,json2])=>{
-        table.querySelectorAll(".odd_normal,.even_normal").forEach((tr, i) => {
-        subject = tr.querySelector("td:nth-child(2)").textContent.replace(/(\d[A-Z]\d+,?)+/, '').trim()
-        td = document.createElement("td")
-        item = json1.find((item)=>{
-            return item['授業科目名'] === subject
-        })
-        if(!item) {item = json2.find((item)=>{
-            return item['授業科目名'] === subject
-        })
-        }
-        if(!item) return
-        td.textContent = String(item["単位取得率"]).substr(0, 4) + "(" + item["単位取得者数（平均）"] + "/" + item["履修登録者数（平均）"] + ")"
-        tr.appendChild(td)
+    Promise.all([
+    fetch(chrome.runtime.getURL("./2018.json")),
+    fetch(chrome.runtime.getURL("./2017.json"))])
+    .then(([res1, res2])=>{
+        return Promise.all([res1.json(), res2.json()])
     })
-})
+    .then(([json1,json2])=>{
+            table.querySelectorAll(".odd_normal,.even_normal").forEach((tr, i) => {
+            subject = tr.querySelector("td:nth-child(2)").textContent.replace(/(\d[A-Z]\d+,?)+/, '').trim()
+            td = document.createElement("td")
+            item = json1.find((item)=>{
+                return item['授業科目名'] === subject
+            })
+            if(!item) {item = json2.find((item)=>{
+                return item['授業科目名'] === subject
+            })
+            }
+            if(!item) return
+            td.textContent = String(item["単位取得率"]).substr(0, 4) + "(" + item["単位取得者数（平均）"] + "/" + item["履修登録者数（平均）"] + ")"
+            tr.appendChild(td)
+        })
+    })
+}
